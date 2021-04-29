023851f02b1de9994f206e22a241d248bdedbd06
Cherno Badjan <badjancherno@gmail.com>
2021-04-29 14:11:04 -0700

    css+updated URL

--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
 chrome.omnibox.onInputEntered.addListener(function (destination) {
   const city = destination.split(",")[0];
   const state = destination.split(", ")[1] || destination.split(",")[1];
-  var newURL = `http://localhost:3000/?city=${city}&state=${state}`;
+  var newURL = `https://destinare-fe.herokuapp.com/?city=${city}&state=${state}`;
   chrome.tabs.create({ url: newURL });
 });
