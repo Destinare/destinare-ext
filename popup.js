3ace0e44f01b2c005de83f6f8b3f51e315373104
Cherno Badjan <badjancherno@gmail.com>
2021-04-29 13:21:54 -0700

    ext+omnibox fixed

--- popup.js
+++ popup.js
@@ -1,15 +1,6 @@
-const newButton = document.getElementById('button');
-
-document.querySelector('body').appendChild(newButton);
-
-newButton.addEventListener('click', () => {
-    const url = 'http://localhost:3000/';
-    chrome.tabs.create({url: url});
-})
-
-
-console.log('outside');
-chrome.omnibox.onInputEntered.addListener(function(text) {
-    var newURL = `https:/localhost:3000/${text}`;
-    chrome.tabs.create({ url: newURL });
-  });
+chrome.omnibox.onInputEntered.addListener(function (destination) {
+  const city = destination.split(",")[0];
+  const state = destination.split(", ")[1] || destination.split(",")[1];
+  var newURL = `http://localhost:3000/?city=${city}&state=${state}`;
+  chrome.tabs.create({ url: newURL });
+});
